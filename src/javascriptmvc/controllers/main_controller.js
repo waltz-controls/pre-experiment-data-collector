f358a60794163988852374313a89a001801f2fdd
Ingvord <mail@ingvord.ru>
2014-02-03 11:28:18 +0100

    move jmvc to src

---
+++ src/javascriptmvc/controllers/main_controller.js
@@ -0,0 +1,18 @@
+MainController = MVC.Controller.extend('main',
+/* @Static */
+{
+    toggleLoading  : function () {
+        $('#loading-box').toggle();
+        $('#example-2').toggle();
+    }
+},
+/* @Prototype */
+{
+    load:function(params){
+        this.Class.toggleLoading();
+
+        var welcome = new WelcomeController();
+        document.getElementById("example-2").innerHTML = welcome.toHtml();
+    }
+}
+);
