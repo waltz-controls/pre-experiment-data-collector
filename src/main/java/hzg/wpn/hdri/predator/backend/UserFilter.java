c90cc89e063ffebc191ad83425866f70f3faf4d1
Ingvord <mail@ingvord.ru>
2014-02-21 14:43:06 +0100

    #15 progress

--- src/main/java/hzg/wpn/hdri/predator/backend/UserFilter.java
+++ src/main/java/hzg/wpn/hdri/predator/backend/UserFilter.java
@@ -1,7 +1,5 @@
 package hzg.wpn.hdri.predator.backend;
 
-import hzg.wpn.hdri.predator.data.User;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -9,6 +7,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
+ * This filter ensures that requests from one user will be processed sequentially in FIFO order
+ *
  * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
  * @since 21.02.14
  */
@@ -23,9 +23,9 @@ public class UserFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String userName = ((HttpServletRequest)request).getRemoteUser();
-        Object token = getOrCreate(userName);
+        Object userLock = getOrCreate(userName);
         //ensure FIFO requests order
-        synchronized (token){
+        synchronized (userLock){
             chain.doFilter(request,response);
         }
     }
