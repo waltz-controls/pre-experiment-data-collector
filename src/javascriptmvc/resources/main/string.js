5759c70befe3da997e90b86b7d2431806e92806b
Ingvord <mail@ingvord.ru>
2014-03-04 12:44:28 +0100

    move jmvc to a dedicated module

--- src/javascriptmvc/resources/main/string.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by
- * User: khokhria
- * Date: 13.01.12
- */
-String.prototype.contains = function(value) {
-    if ((typeof value).toLowerCase() != 'string') return false;
-    return this.indexOf(value) > -1;
-};
